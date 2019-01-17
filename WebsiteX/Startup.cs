ece60eba6671c7b1aae4bb2dbdecc6b5ccb775d3
yuan cheng <chengyuan82281681@hotmail.com>
2019-01-17 11:59:27 +1100

    Add project files.

---
+++ WebsiteX/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebsiteX.Startup))]
+namespace WebsiteX
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
